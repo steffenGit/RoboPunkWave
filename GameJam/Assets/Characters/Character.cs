4afc94e8ba941c811b271de0619ba9959b3cd9e0
TimmyTimsen <tim@hable-net.com>
2017-01-21 18:00:59 +0100

    OHD, Character class

---
+++ GameJam/Assets/Characters/Character.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : ICharacter
+{
+    protected int maximumLifepoints;
+    protected int currentLifepoints;
+    protected string name;
+    protected int defence;
+    protected int speed;
+    protected int attack;
+
+    public Character(int maximumHitpoints, 
+        int defence, 
+        int speed, 
+        string name,
+        int attack)
+    {
+        this.maximumLifepoints 
+            = this.currentLifepoints 
+            = maximumHitpoints;
+        this.name = name;
+        if (defence <= 50)  //max defence is 50
+        {
+            this.defence = defence;
+        }
+        else
+        {
+            this.defence = 50;
+        }
+        this.speed = speed;
+        this.attack = attack;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+
+    public int getDefence()
+    {
+        return this.defence;
+    }
+
+
+    public int getMaximumLifepoints()
+    {
+        return this.maximumLifepoints;
+    }
+
+    public int getCurrentLifepoints()
+    {
+        return this.currentLifepoints;
+    }
+
+    public int incrementLifepoints(int lifepoints)
+    {
+        if (isAlive())
+        {
+            this.currentLifepoints = this.currentLifepoints + lifepoints;
+            return this.currentLifepoints;
+        }
+        else
+        {
+            return currentLifepoints;
+        }
+    }
+
+    public int decrementlifepoints(int hitpoints)
+    {
+        this.currentLifepoints = this.currentLifepoints - ((int)(hitpoints * this.defence / 100) + 1);
+        return this.currentLifepoints;
+    }
+   
+    public bool isAlive()
+    {
+        return this.maximumLifepoints - this.currentLifepoints > 0;
+    }
+
+   public int getAttackValue(ATTACKS.attacks attack)
+    {
+        switch(attack)
+        {
+            case ATTACKS.attacks.WAVE_PUNCH:
+                return ATTACKS.WAVE_PUNCH_DMG + this.attack;
+            case ATTACKS.attacks.SHOCK_WAVE:
+                return ATTACKS.SHOCK_WAVE_DMG + this.attack;
+            case ATTACKS.attacks.KICK:
+                return ATTACKS.KICK_DMG + this.attack;
+            case ATTACKS.attacks.PUNCH:
+                return ATTACKS.PUNCH_DMG + this.attack;
+            default:
+                return 0;
+        }
+    } 
+
+    public void collidedWithElementOfDeath()
+    {
+        this.currentLifepoints = 0;
+    }
+}
---
+++ GameJam/Assets/Characters/HealthBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    private Image content;
+
+    [SerializeField]
+    private Text name;
+
+    [SerializeField]
+    private Text lifepoints;
+
+    private Character character;
+
+    public HealthBar(Character character)
+    {
+        this.character = character;
+        this.name.text = character.getName();
+        this.lifepoints.text = character.getMaximumLifepoints().ToString();
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleBar();
+	}
+
+    private void handleBar()
+    {
+        content.fillAmount = 
+            (float)this.character.getCurrentLifepoints() 
+            / (float)this.character.getMaximumLifepoints();
+
+        this.lifepoints.text = character.getCurrentLifepoints().ToString();
+    }
+
+
+
+}
