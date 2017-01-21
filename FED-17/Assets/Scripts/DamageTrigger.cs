92295b54597cb2af5e3d5195ec952cc4cc97f74b
Kloeti666 <kloeti666@googlemail.com>
2017-01-21 17:14:58 +0100

    initial upload

---
+++ FED-17/Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+	public int ownPlayerId;
+	public enum eDamageTrigger { Punch, Kick, UpperCut, CircleKick, LowerKick };
+
+	public eDamageTrigger triggerType;
+	PlayControllerScript controller_;
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		if( !controller_ )
+		{
+			Debug.LogError("Error: PlayControllerScript not set!");
+			return;
+		}
+
+		if( !(other.tag == "Player") )
+			return;
+
+        int collPlayerId = other.GetComponent<PlayControllerScript>().GetPlayerId();
+		if( collPlayerId != ownPlayerId )
+		{
+			controller_.DamageTriggerCallback( (int)triggerType, other );
+		}
+    }
+	
+	public void SetCallback( PlayControllerScript controller )
+	{
+		controller_ = controller;
+		ownPlayerId = controller.GetComponent<PlayControllerScript>().GetPlayerId();
+	}
+}
