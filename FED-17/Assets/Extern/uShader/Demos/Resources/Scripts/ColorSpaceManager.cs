3275c4290221569b6c8ee562862e97b3f7a5d27f
Stefen Andresen <andresen.steffen@googlemail.com>
2017-01-21 23:30:47 +0100

    addet level1

---
+++ FED-17/Assets/Extern/uShader/Demos/Resources/Scripts/ColorSpaceManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable, ExecuteInEditMode]
+public class ColorSpaceManager : MonoBehaviour {
+
+    public ColorSpace colorSpace;
+
+	// Use this for initialization
+    void Awake()
+    {
+        UnityEditor.PlayerSettings.colorSpace = colorSpace;
+	}
+}
