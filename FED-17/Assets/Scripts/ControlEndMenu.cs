48c67a8892f99ae6cde9ab16fb7f7953e5b1b452
Johann Kirchkesner <johann.kirchkesner@hytera.de>
2017-01-22 12:25:25 +0100

    add EndMenu to TestLevel

---
+++ FED-17/Assets/Scripts/ControlEndMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ControlEndMenu : MonoBehaviour
+{
+    [SerializeField]
+    public Transform endMenu;
+
+    public Text winText;
+
+    public void OpenMenu(int playerNumber)
+    {
+        endMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+        winText.text = "Player " + playerNumber + " wins!";
+    }
+
+    public void LoadMainMenu()
+    {
+        endMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartLevel(int levelId)
+    {
+        endMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(levelId);
+    }
+}
